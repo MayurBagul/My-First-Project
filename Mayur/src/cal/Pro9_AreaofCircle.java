925eb73f37a316bb4b1bf60162185cf3f17e2fc4
Admin <Admin@Admin-PC>
2018-09-04 16:21:47 +0530

    new class i have created today which connect database with oracle. second programs is calculating parameter and area of circle.

---
+++ Mayur/src/cal/Pro9_AreaofCircle.java
@@ -0,0 +1,20 @@
+package cal;
+
+public class Pro9_AreaofCircle {
+
+	public static void main(String[] args) 
+	{
+		float radius=5.2f;
+		
+		double parameter=2*Math.PI*radius;
+		
+		double area=Math.PI*radius*radius;
+		
+		
+		System.out.println("Perimeter of Circle which having Radius 5.2 is = "+parameter);
+		
+		System.out.println("Area of Circle which having Radius 5.2 is = "+area);
+		
+	}
+
+}
---
+++ Mayur/src/dao/ConnectOrac.java
@@ -0,0 +1,52 @@
+package dao;
+
+import java.sql.*;
+
+public class ConnectOrac 
+{
+	Connection con;
+	public Connection connect()
+	{
+	
+		try 
+		{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","Mayur");
+		
+		System.out.println("Connection Established");
+		
+		con.close();
+		System.out.println("Connection closed");
+		
+		}
+		
+	
+		catch (ClassNotFoundException e) 
+		{
+		e.printStackTrace();
+		}
+	
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		return con;
+	
+		
+	
+	}
+
+public static void main(String args[])
+{
+	ConnectOrac c= new ConnectOrac();
+	
+	c.connect();
+}
+
+}
+
